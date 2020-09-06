7bd755883e7f488f31f8cdd28695dfa0b9a89aa3
j2eedev-imran <helloimran24h@gmail.com>
2020-09-06 19:40:33 -0400

    kafka producer is added

---
+++ springboot-kafkaproducer/src/main/java/com/example/kafkaproducer/dto/User.java
@@ -0,0 +1,16 @@
+package com.example.kafkaproducer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	private Integer id;
+	private String userName;
+	private String[] addresses;
+
+}
